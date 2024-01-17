9fc71d7cee441fd8ec505db823d09b88456e5f6f
Sonny T. <mail@sonnyt.com>
2024-01-17 21:02:30 +0000

    this is bothering me

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -10,38 +10,40 @@ export default function Editor() {
   const [state, dispatch] = useConsole();
 
   useEffect(() => {
-    if (monaco) {
-      monaco.editor.defineTheme("onedark-pro", darkTheme as any);
-      monaco.editor.setTheme("onedark-pro");
-
-      monaco.editor.addEditorAction({
-        id: "execute_code",
-        label: "Execute Code",
-        keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter],
-        contextMenuGroupId: "navigation",
-        contextMenuOrder: 0,
-        run: (editor) => {
-          const selectedCode = editor
-            .getModel()
-            ?.getValueInRange(editor?.getSelection()!);
-
-          if (selectedCode?.trim()) {
-            dispatch({ type: "RUN_CODE", payload: { selectedCode } });
-          } else {
-            dispatch({ type: "RUN_CODE" });
-          }
-        },
-      });
-
-      monaco.editor.addEditorAction({
-        id: "clear_console",
-        label: "Clear Console",
-        keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyK],
-        contextMenuGroupId: "navigation",
-        contextMenuOrder: 1,
-        run: () => dispatch({ type: "CLEAR_LOGS" }),
-      });
+    if (!monaco) {
+      return;
     }
+
+    monaco.editor.defineTheme("onedark-pro", darkTheme as any);
+    monaco.editor.setTheme("onedark-pro");
+
+    monaco.editor.addEditorAction({
+      id: "execute_code",
+      label: "Execute Code",
+      keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter],
+      contextMenuGroupId: "navigation",
+      contextMenuOrder: 0,
+      run: (editor) => {
+        const selectedCode = editor
+          .getModel()
+          ?.getValueInRange(editor?.getSelection()!);
+
+        if (selectedCode?.trim()) {
+          dispatch({ type: "RUN_CODE", payload: { selectedCode } });
+        } else {
+          dispatch({ type: "RUN_CODE" });
+        }
+      },
+    });
+
+    monaco.editor.addEditorAction({
+      id: "clear_console",
+      label: "Clear Console",
+      keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyK],
+      contextMenuGroupId: "navigation",
+      contextMenuOrder: 1,
+      run: () => dispatch({ type: "CLEAR_LOGS" }),
+    });
   }, [monaco, dispatch]);
 
   return (
