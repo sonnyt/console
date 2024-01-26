0eb995c6c6be6883460688826d08bf73d14b3a38
Sonny T. <mail@sonnyt.com>
2024-01-26 21:23:52 +0000

    split up

--- src/components/log-item.tsx
+++ src/components/log-item.tsx
@@ -5,41 +5,6 @@ import Icons from "./icons";
 import { getObjectName, getType, parseFunction } from "../libs/utils";
 import { NodeTypes, ValueTypes } from "../libs/constants";
 
-const Collapsable = ({
-  children,
-  toggle = false,
-  ...props
-}: React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement> & { toggle?: boolean }>) => {
-  const [isCollapsed, setIsCollapsed] = useState<boolean>(true);
-
-  const onToggle = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.stopPropagation();
-    setIsCollapsed(!isCollapsed);
-  };
-
-  const components = React.Children.toArray(children);
-
-  return (
-    <div
-      {...props}
-      className={`${styles.log} ${props.className} ${styles.collapse}`}
-      onClick={onToggle}
-    >
-      {isCollapsed ? (
-        <Icons.Right width={14} height={14} />
-      ) : (
-        <Icons.Down width={14} height={14} />
-      )}
-      {toggle ? (isCollapsed ? components[0] : components[1]) : (
-        <>
-          {components[0]}
-          {!isCollapsed && components[1]}
-        </>
-      )}
-    </div>
-  );
-};
-
 const StringLog = ({ log, ...props }: React.HTMLAttributes<HTMLDivElement> & { log: any }) => {
   return (
     <div {...props} className={`${styles.log} ${props.className ?? ''}`}>{log.toString()}</div>
